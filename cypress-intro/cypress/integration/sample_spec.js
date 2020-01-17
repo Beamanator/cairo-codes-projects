a7b143fe39e4631c2ba97aaac4d5151d2e66afde
Alex Beaman <alex@insidenewcity.com>
2020-01-17 21:24:29 +0200

    tests: adding tests written in class

--- cypress-intro/cypress/integration/sample_spec.js
+++ cypress-intro/cypress/integration/sample_spec.js
@@ -4,3 +4,45 @@ describe("My First Test", function() {
         expect(true).to.equal(true);
     });
 });
+
+// tests written in class on
+describe("CC favorite test group", () => {
+    it("runs sample tests", () => {
+        expect(true).to.be.true;
+        expect([1, 2, 3]).to.include(3);
+    });
+
+    it("renders App div", () => {
+        cy.visit("http://localhost:3000/");
+        cy.get("div.App");
+    });
+
+    it("renders main text", () => {
+        cy.visit("http://localhost:3000/");
+        cy.get("p.main-text");
+    });
+
+    it("renders the react icon", () => {
+        cy.visit("http://localhost:3000/");
+        cy.get("img.App-logo");
+    });
+
+    it("renders the Learn React link", () => {
+        cy.visit("http://localhost:3000/");
+
+        // cy.get("a.App-link").contains("Learn React");
+        cy.get("a.App-link").then(($el) => {
+            const text = $el.text();
+
+            expect(text).to.include("Learn React");
+            expect(text).to.not.include("poopy");
+            expect(text).to.not.include("butthole");
+        });
+    });
+
+    it("shows the default react app page title", () => {
+        // cy.title().contains("React App");
+        cy.title().should("eq", "React App");
+        cy.title().should("includes", "App");
+    });
+});
